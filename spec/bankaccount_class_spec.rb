4d123aa866c31aa9d180dcdd70aacc26d3b6596f
Dalton Mann <daltonjamann@gmail.com>
2024-02-22 14:42:00 -0600

    Completed problem 4

---
+++ spec/bankaccount_class_spec.rb
@@ -0,0 +1,30 @@
+RSpec.describe BankAccount do
+  describe "#deposit" do
+    it "increases the balance by the deposit amount" do
+      account = BankAccount.new(500)
+      account.deposit(50)
+      expect(account.balance).to eq(550)
+    end
+  end
+
+  describe "#withdraw" do
+    it "decreases the balance by the withdrawal amount if funds are available" do
+      account = BankAccount.new(500)
+      account.withdraw(50)
+      expect(account.balance).to eq(450)
+    end
+
+    it "does not change the balance if insufficient funds" do
+      account = BankAccount.new(50)
+      account.withdraw(500)
+      expect(account.balance).to eq(50)
+    end
+  end
+
+  describe "#balance" do
+    it "returns the current balance" do
+      account = BankAccount.new(500)
+      expect(account.balance).to eq(500)
+    end
+  end
+end
